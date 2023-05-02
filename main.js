56cafe27f8c0371afc5cc8768482263b81986df5
samgjenkins <129430461+samgjenkins@users.noreply.github.com>
2023-05-01 17:29:09 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $.getJSON('http://api.open-notify.org/astros.json?callback=?', function(data) {
     var number = data['number'];
     $('#SpacePeople').html(number);
 
-    let d = data['craft']
+//     let d = data['craft']
 
     data['people'].forEach(function (d) {
         $('#spaceships').prepend('<img id="rocket" src="images/rocketOG.png">');
